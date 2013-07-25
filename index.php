fa1635917b7bb66e38cfcd416efaab678bbbf508
Jo Greybill <jogreybill@gmail.com>
2013-07-25 12:26:30 -0400

    Removed extraneous landmark attribute

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php if (function_exists('disallow_direct_load')):?>
 <?php disallow_direct_load('index.php');?>
 <?php get_header();?>
-<div class="page-content" id="post-list" role="">
+<div class="page-content" id="post-list">
 	<div class="span12" role="main">
 		<?php while(have_posts()): the_post();?>
 		<article class="<?=$post->post_status?>">
