1632db2cd1f43254f5a51f45d9f3bcf7d6936801
Jo Greybill <jogreybill@gmail.com>
2013-11-20 10:04:59 -0500

    Updated html5 shiv to be protocol-agnostic

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 		</div>
 	</body>
 	<!--[if IE]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<?="\n".footer_()."\n"?>
 </html>
