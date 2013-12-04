0db3d33869a4d610f5cb13078ed821ac4420bfb5
Jo Greybill <jogreybill@gmail.com>
2013-12-04 09:39:11 -0500

    Added filter to force upscaling of small images for thumbnail cropping

--- functions.php
+++ functions.php
@@ -799,4 +799,25 @@ function protocol_relative_oembed($html) {
     return preg_replace('@src="https?:@', 'src="', $html);
 }
 add_filter('embed_oembed_html', 'protocol_relative_oembed');
+
+/*
+ * Force an exact crop of an image; bypassing wordpress's default
+ * cropping settings which do not upscale small images.
+ * http://wordpress.stackexchange.com/a/64953
+ */
+function image_crop_dimensions($default, $orig_w, $orig_h, $new_w, $new_h, $crop){
+    if ( !$crop ) return null; // let the wordpress default function handle this
+
+    $aspect_ratio = $orig_w / $orig_h;
+    $size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
+
+    $crop_w = round($new_w / $size_ratio);
+    $crop_h = round($new_h / $size_ratio);
+
+    $s_x = floor( ($orig_w - $crop_w) / 2 );
+    $s_y = floor( ($orig_h - $crop_h) / 2 );
+
+    return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
+}
+add_filter('image_resize_dimensions', 'image_crop_dimensions', 10, 6);
 ?>
