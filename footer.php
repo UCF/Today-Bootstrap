ccb95c3ba7ee250de5609738d501c53cd13fbdbd
RJ Bruneel <rj.bruneel@ucf.edu>
2015-05-05 14:33:10 -0400

    Added footer

--- footer.php
+++ footer.php
@@ -1,19 +1,33 @@
 			<div id="footer">
+				<div id="footer-menu" class="row">
+					<div class="span12 text-center">
+						<a href="http://www.ucf.edu/azindex/">A-Z Index</a> |
+						<a href="http://www.ucf.edu/about-ucf/">About UCF</a> |
+						<a href="http://www.ucf.edu/contact/">Contact UCF</a> |
+						<a href="http://www.ucf.edu/internet-privacy-policy/">Internet Privacy Policy</a> |
+						<a href="http://policies.ucf.edu/" target="_blank" class="external">Policies</a> |
+						<a href="http://regulations.ucf.edu/" target="_blank" class="external">Regulations</a> |
+						<a href="http://today.ucf.edu" target="_blank" class="external">UCF Today</a> |
+						<a href="http://www.ucf.edu/pegasus">Pegasus</a> |
+						<a href="http://m.ucf.edu/" target="_blank" class="external">Mobile Website</a>
+					</div>				
+				</div>
 				<div class="row" id="footer-widget-wrap">
-					<div class="footer-widget-1 span4">
-						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('1st Subsidary Aside')):?>
-							<a href="<?=bloginfo('url')?>"><img src="<?=THEME_IMG_URL?>/ucftoday4_small.png" alt="UCF Today" title="UCF Today" /></a>
-						<?php endif;?>
-					</div>
-					<div class="footer-widget-2 span4">
-						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('2nd Subsidary Aside')):?>
-						&nbsp;
-						<?php endif;?>
-					</div>
-					<div class="footer-widget-3 span4">
-						<?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('3rd Subsidary Aside')):?>
-						&nbsp;
-						<?php endif;?>
+					<div class="span12 text-center">
+						<p id="subfooter" role="contentinfo" class="vcard">
+							<span class="adr">
+								<span class="street-address">4000 Central Florida Blvd. </span>
+								<span class="locality">Orlando</span>, 
+								<span class="region">Florida</span>, 
+								<span class="postal-code">32816</span> | 
+								<span class="tel"><a href="tel:4078232000">407.823.2000</a></span>
+							</span>
+							<br>
+							<a href="http://www.ucf.edu/feedback/">Comments and Feedback</a> | © 
+							<a href="http://www.ucf.edu" class="print-noexpand fn org url">
+								<span class="organization-name">University of Central Florida</span>
+							</a>
+						</p>
 					</div>
 				</div>
 			</div>
