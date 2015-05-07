3ae6817c1a8183b0283ead8f5c27c3c5e6eedb47
RJ Bruneel <rj.bruneel@ucf.edu>
2015-05-07 09:26:42 -0400

    Updated search input stying and footer.

--- footer.php
+++ footer.php
@@ -1,18 +1,4 @@
-			<div id="footer">
-				<div id="footer-menu" class="row">
-					<div class="span12 text-center">
-						<a href="http://www.ucf.edu/azindex/">A-Z Index</a> |
-						<a href="http://www.ucf.edu/about-ucf/">About UCF</a> |
-						<a href="http://www.ucf.edu/contact/">Contact UCF</a> |
-						<a href="http://www.ucf.edu/internet-privacy-policy/">Internet Privacy Policy</a> |
-						<a href="http://policies.ucf.edu/" target="_blank" class="external">Policies</a> |
-						<a href="http://regulations.ucf.edu/" target="_blank" class="external">Regulations</a> |
-						<a href="http://today.ucf.edu" target="_blank" class="external">UCF Today</a> |
-						<a href="http://www.ucf.edu/pegasus">Pegasus</a> |
-						<a href="http://m.ucf.edu/" target="_blank" class="external">Mobile Website</a>
-					</div>				
-				</div>
-				
+			<div id="footer">				
 				<div class="row" id="footer-widget-wrap">
 					<div class="span12 text-center">
 						<p id="subfooter" role="contentinfo" class="vcard">
@@ -24,8 +10,7 @@
 								<span class="tel"><a href="tel:4078232000">407.823.2000</a></span>
 							</span>
 							<br>
-							<a href="http://www.ucf.edu/feedback/">Comments and Feedback</a> | © 
-							<a href="http://www.ucf.edu" class="print-noexpand fn org url">
+							© <a href="http://www.ucf.edu" class="print-noexpand fn org url">
 								<span class="organization-name">University of Central Florida</span>
 							</a>
 						</p>
