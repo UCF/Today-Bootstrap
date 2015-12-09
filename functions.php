0266319a4638c37e3b640c497784a0ea7b18f49e
Jo Dickson <jogreybill@gmail.com>
2015-12-09 09:52:17 -0600

    Flipped order for old cat slug rewrite rules; updated /category/... rules for old cats to reference index.php?tag= instead of index.php?category_name for consistency

--- functions.php
+++ functions.php
@@ -51,10 +51,10 @@ function transition_rules()
 		$custom['category/(?:[^/]+/)?'.$before.'/?$'] = 'index.php?tag='.$after;
 
 		// Rewrite feed pages
-		$custom['section/(?:[^/]+/)?'.$before.'/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?tag='.$after.'&feed=$matches[1]';
 		$custom['section/(?:[^/]+/)?'.$before.'/feed/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?tag='.$after.'&feed=$matches[1]';
-		$custom['category/(?:[^/]+/)?'.$before.'/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?category_name='.$after.'&feed=$matches[1]';
-		$custom['category/(?:[^/]+/)?'.$before.'/feed/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?category_name='.$after.'&feed=$matches[1]';
+		$custom['section/(?:[^/]+/)?'.$before.'/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?tag='.$after.'&feed=$matches[1]';
+		$custom['category/(?:[^/]+/)?'.$before.'/feed/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?tag='.$after.'&feed=$matches[1]';
+		$custom['category/(?:[^/]+/)?'.$before.'/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?tag='.$after.'&feed=$matches[1]';
 	}
 	// Rewrite old category and tag pages
 	$custom['category/(?:[^/]+/)?(.+?)/feed/(feed|rdf|rss|rss2|atom|json)/?$'] = 'index.php?category_name=$matches[1]&feed=$matches[2]';
