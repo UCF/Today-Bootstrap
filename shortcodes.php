f980794206330a0cc61bb5ccd2ff8431f4112812
Cadie <cadie.brown@gmail.com>
2018-02-06 14:15:06 -0500

    use output buffers instead of concatenating HTML in  var

--- shortcodes.php
+++ shortcodes.php
@@ -228,18 +228,24 @@ function sc_feature($atts = Array(), $id_only = False)
 				$attachment_url = array(0 => THEME_IMG_URL.'/no-photo.png');
 			}
 
-			if($video_url != '') {
-				$feature_media = '<div class="video-container" style="background-image: url('.$attachment_url[0].');">'
-									.$wp_embed->run_shortcode('[embed width="417" height="343"]'.$video_url.'[/embed]').
-								'</div>';
-			} else {
-				$feature_media = '<div class="thumb cropped" style="background-image: url('.$attachment_url[0].');">
-									<a href="'.get_permalink($feature->ID).'">
-										'.get_img_html($feature->ID, 'feature').
-									'</a>
-								</div>';
+			ob_start();
 
-			}
+			if($video_url != '') :
+			?>
+			<div class="video-container" style="background-image: url('.$attachment_url[0].');">'
+				<?=$wp_embed->run_shortcode('[embed width="417" height="343"]'.$video_url.'[/embed]')?>
+			</div>
+			<? else :
+			?>
+			<div class="thumb cropped" style="background-image: url('.$attachment_url[0].');">
+				<a href="'.get_permalink($feature->ID).'">
+					<?=get_img_html($feature->ID, 'feature')?>
+				</a>
+			</div>
+			<?
+			endif;
+
+			$feature_media = ob_get_clean();
 
 			ob_start();
 			?>
