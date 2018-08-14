0d8792075a54d042d9e3188e589e5f3bc5819437
Cadie Brown <cadie.brown@gmail.com>
2018-08-14 10:04:55 -0400

    Add show_description attr for external_stories sc and clean markup Show descriptions for links displayed using the external_stories sc if the show_descriptions attr is true.

--- shortcodes.php
+++ shortcodes.php
@@ -856,35 +856,43 @@ add_shortcode('update', 'sc_update');
  * @return string
  * @author Chris Conover
  **/
-function sc_external_stories($atts = Array())
-{
+function sc_external_stories( $atts = array() ) {
 	global $wp_query;
 
-	$css = (isset($atts['css'])) ? $atts['css'] : '';
+	$css = ( isset( $atts['css'] ) ) ? $atts['css'] : '';
 	$heading = ( isset( $atts['heading'] ) ) ? $atts['heading'] : 'UCF in the News';
 	$number_links = ( isset( $atts['number_links'] ) && is_numeric( $atts['number_links'] ) ) ? intval( $atts['number_links'] ) : 4;
 	$link_page_name = ( isset( $atts['link_page_name'] ) ) ? $atts['link_page_name'] : 'UCF in the News';
+	$show_description = filter_var( $atts['show_description'], FILTER_VALIDATE_BOOLEAN );
+	$show_description = ( isset( $atts['show_description'] ) && $show_description ) ? $show_description : false;
 
 	$stories = resolve_posts(	Array(	'tag' => $wp_query->queried_object->slug),
 								Array(	'post_type' => 'externalstory',
 										'numberposts' => $number_links ) );
-	if(count($stories) > 0) {
+	if ( count( $stories ) > 0 ) {
 		ob_start();
 		?>
-		<div class="<?=$css?>" id="external_stories">
+		<div class="<?php echo $css; ?>" id="external_stories">
 			<h2><?php echo $heading; ?></h2>
 			<ul class="story-list">
-				<? foreach($stories as $story) { ?>
+				<?php foreach ( $stories as $story ) :
+					$story_url = get_post_meta( $story->ID, 'externalstory_url', True );
+					$story_text = get_post_meta( $story->ID, 'externalstory_text', True );
+					$story_source = get_post_meta( $story->ID, 'externalstory_source', True );
+					$story_description = get_post_meta( $story->ID, 'externalstory_description', True );
+				?>
 					<li>
-						<a href="<?=get_post_meta($story->ID, 'externalstory_url', True)?>">
-							<?=get_post_meta($story->ID, 'externalstory_text', True)?>
+						<a href="<?php echo $story_url; ?>">
+							<?php echo $story_text; ?>
 						</a>
-						<span><?=get_post_meta($story->ID, 'externalstory_source', True)?></span>
+						<?php if ( $show_description && !empty( $story_description ) ) : ?>
+							<p><?php echo $story_description; ?></p>
+						<?php endif; ?>
+						<span><?php echo $story_source; ?></span>
 					</li>
-				<? } ?>
+				<?php endforeach; ?>
 			</ul>
 			<a href="<?php echo get_page_link( get_page_by_title( $link_page_name )->ID ); ?>" class="external-stories-view-all">View All &raquo;</a>
-
 		</div>
 		<?
 		$html = ob_get_contents(); ob_end_clean();
