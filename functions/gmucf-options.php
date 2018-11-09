a44dd48cf40a65b78d938a12b1a1d4e57cac6c81
Cadie Brown <cadie.brown@gmail.com>
2018-11-09 17:35:55 -0500

    Added rest api for gmucf options

--- functions/gmucf-options.php
+++ functions/gmucf-options.php
@@ -16,3 +16,33 @@ if ( function_exists('acf_add_options_page') ) {
 	));
 
 }
+
+/**
+ * Sets the default values for each stories image, title and
+ * description for use in the GMUCF Today emails
+ * @since 2.9.0
+ * @author Cadie Brown
+ * @param Array $stories | Stories array from ACF GMUCF Options Page
+ * @return Array
+ */
+function gmucf_stories_default_values( $stories ) {
+	foreach ( $stories as $story ) {
+		$post_id = $story['gmucf_story'];
+
+		if ( ! $story['gmucf_story_image'] ) {
+			$story['gmucf_story_image'] = get_the_post_thumbnail_url( $post_id );
+		}
+
+		if ( ! $story['gmucf_story_title'] ) {
+			$story['gmucf_story_title'] = get_the_title( $post_id );
+		}
+
+		if ( ! $story['gmucf_story_description'] ) {
+			$story['gmucf_story_description'] = get_post_meta( $post_id, 'promo', true );
+		}
+
+		$retval[] = $story;
+	}
+
+	return $retval;
+}
