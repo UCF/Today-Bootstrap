92708bb851e22ebc982e67b935ac9c13319d05b3
RJ Bruneel <rj.bruneel@ucf.edu>
2018-12-14 11:17:54 -0500

    PHP Style guide updates

--- page-news-archive.php
+++ page-news-archive.php
@@ -1,12 +1,12 @@
-<?php get_header();?>
+<?php get_header(); ?>
     <div id="archives" class="subpage">
         <div class="row">
             <div class="span9 border-right">
-                <?=do_shortcode('[sc-archive-articles]')?>
+                <?php echo do_shortcode( '[sc-archive-articles]' ); ?>
             </div>
             <div class="span3" id="sidebar">
-                <?=esi_include('do_shortcode', '[events]', true)?>
+                <?php echo esi_include( 'do_shortcode', '[events]', true ); ?>
             </div>
         </div>
     </div>
-<?php get_footer();?>
+<?php get_footer(); ?>
