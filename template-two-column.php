fd6d3159ee481f86d9c923fe49599c042e16e825
Cadie Brown <cadie.brown@gmail.com>
2018-12-14 10:15:55 -0500

    PHP Style guide updates

--- template-two-column.php
+++ template-two-column.php
@@ -4,15 +4,15 @@
  **/
 ?>
 <?php get_header(); the_post();?>
-	<div class="row page-content" id="<?=$post->post_name?>">
+	<div class="row page-content" id="<?php echo $post->post_name; ?>">
 		<div class="span9">
 			<article role="main">
-				<?php the_content();?>
+				<?php the_content(); ?>
 			</article>
 		</div>
 
 		<div id="sidebar" class="span3" role="complementary">
-			<?=get_sidebar();?>
+			<?php echo get_sidebar(); ?>
 		</div>
 	</div>
 <?php get_footer();?>
