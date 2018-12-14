69fa85c306d331a1b7101c0b3ac75d443cdf60f9
RJ Bruneel <rj.bruneel@ucf.edu>
2018-12-14 11:49:40 -0500

    PHP Style guide updates

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <div class="search">
-	<form role="search" method="get" class="search-form" action="<?=home_url( '/' )?>">
+	<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 		<label for="s">Search:</label>
-		<input type="text" value="<?=htmlentities($_GET['s'])?>" name="s" class="search-field" id="s" placeholder="Search UCF Today" />
+		<input type="text" value="<?php echo htmlentities( $_GET['s'] ); ?>" name="s" class="search-field" id="s" placeholder="Search UCF Today" />
 		<button type="submit" class="search-submit"><span class="sr-only">Search</span><span class="fa fa-search" aria-hidden="true"></span></button>
 	</form>
 </div>
